88b9c8c0102c3798205ccff318d6b8df7cbbfa11
Karlo Lozovina <karlo@sklogw.com>
2024-10-24 11:55:16 +0200

    Dewey: styling, empty directories

--- dewey/src/dewey/App.kt
+++ dewey/src/dewey/App.kt
@@ -1,5 +1,6 @@
 package dewey
 
+import org.gnome.gdk.Display
 import org.gnome.gdk.Gdk
 import org.gnome.gdk.ModifierType
 import org.gnome.gio.ApplicationFlags
@@ -11,40 +12,52 @@ import kotlin.io.path.*
 
 val homeDirectory = Path(System.getProperty("user.home"))
 
-class DirectoryBrowser(var path: Path) {
+val styleCss = """
+    .directory {
+        color: #ff0000;
+    }
+    .file {
+        color: #0000ff;
+    }
+    .empty {
+        color: #000000;
+        font-style: italic;
+    }
+"""
+
+class DirectoryBrowser(path: Path) {
 
     private var state = State(path)
-
-
-    private var directoryList = path.listDirectoryEntries()
-
-    private var directoryListModel = StringList(directoryList.map { pathToString(it) }.toTypedArray())
-    private var selectionModel = SingleSelection(directoryListModel)
     private val itemFactory = ItemFactory()
 
-
-    private val eventController = EventControllerKey()
-    val widget = ListView(selectionModel, itemFactory)
-
-    init {
-        widget.onActivate(::activateHandler)
-        eventController.onKeyPressed(::keyPressHandler)
-        widget.addController(eventController)
+    private val eventController = EventControllerKey().apply {
+        onKeyPressed(::keyPressHandler)
     }
 
+    val widget = ListView(state.selectionModel, itemFactory).apply {
+        onActivate(::activateHandler)
+        addController(eventController)
+    }
+
+    /**
+     * Handle keyboard shortcuts in browser.
+     */
     private fun keyPressHandler(keyVal: Int, keyCode: Int, modifierTypes: MutableSet<ModifierType>?): Boolean {
         when (keyVal) {
             Gdk.KEY_Left -> navigateToParent()
-            Gdk.KEY_Right -> widget.emitActivate(selectionModel.selected)
+            Gdk.KEY_Right -> widget.emitActivate(state.selectionModel.selected)
         }
         println("> Key: [$keyVal]: ${Gdk.keyvalName(keyVal)}, $keyCode, $modifierTypes")
         return false
     }
 
+    /**
+     * Handle activating an item in the browser.
+     */
     private fun activateHandler(idx: Int) {
-        val activatedPath = directoryList[idx]
+        val activatedPath = state.dirList[idx]
 
-        // Skip files
+        // Skip files when activated
         if (!activatedPath.isDirectory())
             return
 
@@ -52,30 +65,35 @@ class DirectoryBrowser(var path: Path) {
         navigateTo(activatedPath)
     }
 
+    /**
+     * Go to new directory and show its contents.
+     */
     private fun navigateTo(target: Path) {
-        println("Navigating to: ${target}")
-        path = target
-        directoryList = path.listDirectoryEntries()
-        directoryListModel = StringList(directoryList.map { pathToString(it) }.toTypedArray())
-        selectionModel = SingleSelection(directoryListModel)
-        widget.model = selectionModel
+        println("Navigating to directory: ${target}")
+        state = State(target)
+        widget.model = state.selectionModel
     }
 
+    /**
+     * Go up in directory hierarchy.
+     */
     private fun navigateToParent() {
-        val parent = path.parent ?: return
+        val parent = state.path.parent ?: return // Handle when showing root
         navigateTo(parent)
     }
 
-    private fun pathToString(path: Path): String =
-        if (path.isDirectory())
-            "[[${path.toString()}]]"
-        else
-            path.toString()
-
-    // TODO: Use this class to keep everything in one place, don't mutate, recreate
+    /**
+     * Keep the state related to single directory view in one place. Don't mutate, recreate.
+     */
     class State(val path: Path) {
-        val dirList = path.listDirectoryEntries()
-        val dirListModel = StringList(dirList.map { pathToString(it) }.toTypedArray())
+        val dirList = path.listDirectoryEntries().sortedByDescending { it.isDirectory() }
+        val isEmpty = dirList.isEmpty()
+
+        // Handle empty directories by adding a dummy item to ListView model
+        val dirListModel =
+            if (!isEmpty) StringList(dirList.map { pathToString(it) }.toTypedArray())
+            else StringList(arrayOf("<< empty folder >>"))
+
         val selectionModel = SingleSelection(dirListModel)
 
         private fun pathToString(path: Path): String =
@@ -85,20 +103,39 @@ class DirectoryBrowser(var path: Path) {
                 path.toString()
     }
 
-    class ItemFactory : SignalListItemFactory() {
+
+    /**
+     * Creates and updates rows in virtualized ListView control.
+     */
+    inner class ItemFactory : SignalListItemFactory() {
         init {
+            println(">>> New list item factory factoried <<<")
             onSetup { setup(it as ListItem) }
             onBind { bind(it as ListItem) }
         }
 
         private fun setup(listItem: ListItem) {
             listItem.child = Label("")
+            println("Creating a new label")
         }
 
         private fun bind(listItem: ListItem) {
             val label = listItem.child as Label
             val item = listItem.item as StringObject
+
             label.label = item.string
+            check(label.cssClasses.size <= 1) { "Can't have more than one class set" }
+
+            if (!state.isEmpty) {
+                if (state.dirList[listItem.position].isDirectory())
+                    label.addCssClass("directory")
+                else
+                    label.addCssClass("file")
+            } else {
+                check(state.dirListModel.nItems == 1) { "On empty directory ListView should have only one element inside." }
+                label.addCssClass("empty")
+            }
+            println("Postion: ${listItem.position}")
         }
     }
 }
@@ -106,8 +143,18 @@ class DirectoryBrowser(var path: Path) {
 
 fun main(args: Array<String>) {
     val app = Application("com.sklogw.nylon.Dewey", ApplicationFlags.DEFAULT_FLAGS)
+    val css = CssProvider().apply {
+        onParsingError { section, error -> println("CSS parsing error: $section, ${error.readMessage()}") }
+        loadFromString(styleCss)
+    }
+
+    app.onStartup {
+        println("App startup")
+    }
 
     app.onActivate {
+        println("App activating")
+        StyleContext.addProviderForDisplay(Display.getDefault(), css, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
         val window = ApplicationWindow(app)
         val scrolled = ScrolledWindow()
 
@@ -116,6 +163,10 @@ fun main(args: Array<String>) {
         scrolled.child = browser.widget
         window.child = scrolled
         window.present()
+    }
+
+    app.onShutdown {
+        println("App shutting down")
     }
 
     app.run(args)
